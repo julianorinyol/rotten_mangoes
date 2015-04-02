6d4be47cbb3ae66302133dd7be08423eea44fa46
JulianOrinyol <julianorinyol@gmail.com>
2015-04-01 19:59:38 -0700

    got the admin page viewable only by admins, and pagination is working

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,19 +2,16 @@ class Admin::UsersController < ApplicationController
     before_action :check_if_admin
 
     def index
-      # binding.pry
-      @users = User.all
+      @users = User.all.page(params[:page]).per(3)
     end
 
     private
 
-    def check_if_admin
+    def check_if_admin 
+      return redirect_to movies_path unless current_user
       id = session[:user_id]
       unless User.find(id).admin
-        # !id || !User.find(id).admin?
-        redirect_to movies_path 
+        redirect_to movies_path
       end
-
     end
-
 end
