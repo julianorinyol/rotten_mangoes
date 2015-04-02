3d9efb39b11b0dae97df530b539b327dca0f0ea4
JulianOrinyol <julianorinyol@gmail.com>
2015-04-01 18:10:43 -0700

    normal users cannot access the admin page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,20 @@
 class Admin::UsersController < ApplicationController
+    before_action :check_if_admin
+
     def index
+      # binding.pry
       @users = User.all
     end
+
+    private
+
+    def check_if_admin
+      id = session[:user_id]
+      unless User.find(id).admin
+        # !id || !User.find(id).admin?
+        redirect_to movies_path 
+      end
+
+    end
+
 end
