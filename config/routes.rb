01ffe5ae9065afe13a577a2d21a0a4f3f2138fbf
JulianOrinyol <julianorinyol@gmail.com>
2015-04-03 13:53:03 -0700

    changed session to a singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
     resources :users
   end 
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session
+
   root to: 'movies#index'
 end
 
+  # resources :sessions, only: [:new, :create, :destroy]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
