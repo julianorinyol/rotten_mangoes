5624ba3e5f0e56a7f8e58529509e669e85e66f63
JulianOrinyol <julianorinyol@gmail.com>
2015-04-03 12:09:38 -0700

    got actionmailer working

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -1,11 +1,9 @@
 class SystemMailer < ApplicationMailer
-  default from "no-reply@rottenmangoes.com"
+  default from: "no-reply@rottenmangoes.com"
 
   def welcome_email(user)
     @user = user
-
+    @url  = 'http://rottenmangoes.com/login'
     mail(to: @user.email, subject: 'Welcome to Rotten Mangoes')
-
-
   end
 end
