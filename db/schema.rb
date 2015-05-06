39cccd24dfa72d404b3fa34e3c28a957be22aba3
JulianOrinyol <julianorinyol@gmail.com>
2015-05-06 13:03:01 -0700

    migrated the db with much difficulty, changed release_date to be an integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401213511) do
+ActiveRecord::Schema.define(version: 20150506125808) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150401213511) do
     t.integer  "runtime_in_minutes"
     t.text     "description"
     t.string   "poster_image_url"
-    t.datetime "release_date"
+    t.integer  "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image"
