d9cedcef990f6e9559bbbeb9d2ae7cfeb8c1f6e2
JulianOrinyol <julianorinyol@gmail.com>
2015-05-05 21:50:25 -0700

    style changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function(){
+  console.log('ready freddy')
+  $('#search-icon').on('click', function(){
+    console.log('search-icon click');
+  })
+})
