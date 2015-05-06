d72a29f8ab2e0834fb5a3b78e892f8874f758611
JulianOrinyol <julianorinyol@gmail.com>
2015-05-06 12:18:15 -0700

    removed validations and the form field for image_url

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,9 +3,9 @@ class Movie < ActiveRecord::Base
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validates :description, presence: true
-  # validates :poster_image_url, presence: true
+  validates :image, presence: true
   validates :release_date, presence: true
-  validate :release_date_is_in_the_future
+ 
 
   has_many :reviews
   
